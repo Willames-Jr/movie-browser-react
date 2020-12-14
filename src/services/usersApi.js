3382cd9a42d28dae94ee16dc0b42b33990f427d1
unknown <willamesj11@gmail.com>
2020-12-13 21:59:47 -0300

    fix db url

--- src/services/usersApi.js
+++ src/services/usersApi.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const usersApi = axios.create({
     // mongodb://localhost:27017/movie-app
     //https://movie-app-serv.herokuapp.com/api/v1/
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://movie-app-serv.herokuapp.com/api/v1/',
     validateStatus: status =>  {
         return status; // default
     },
