58a862c5a079fafaab99daa49766a61da9058e90
willames-jr <willamesj11@gmail.com>
2023-03-18 00:28:10 -0300

    new api server

--- src/services/usersApi.js
+++ src/services/usersApi.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const usersApi = axios.create({
     // http://localhost:5000/api/v1/
     // https://movie-app-serv.herokuapp.com/api/v1/
-    baseURL: 'https://movie-app-serv.herokuapp.com/api/v1/',
+    baseURL: 'https://react-movies-server.herokuapp.com/api/v1/',
     validateStatus: status =>  {
         return status; // default
     },
